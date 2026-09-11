2bd4a56fd3ea0e0e2f943e2bcc6502b93196c5bd
Gabriel <bielemessantos@gmail.com>
2026-09-11 00:50:17 -0300

    Replace grade and attendance check with heart rate monitor
    The program now monitors heart rate (bpm) and classifies it as Normal, Alerta, or Crítico. It tracks the number of readings and stops after three consecutive critical states.

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,41 @@
-m = float(input("Digite a média: "))
-f = float(input("Digite o percentual de frequencia: "))
-if f < 75:
-    print(("Reprovado por falta!"))
-elif m < 6:
-    print("Reprovado por nota!")
-else:
-    print("Aprovado")
+total_leituras = 0
+normal = 0
+alerta = 0
+critico = 0
+criticos_consecutivos = 0
+
+while True:
+    bpm = int(input("Digite a frequência cardíaca (bpm): "))
+
+    total_leituras += 1
+
+    if 60 <= bpm <= 100:
+        print("Classificação: Normal")
+        normal += 1
+        criticos_consecutivos = 0  # zera sequência
+
+    elif 101 <= bpm <= 120:
+        print("Classificação: Alerta")
+        alerta += 1
+        criticos_consecutivos = 0  # zera sequência
+
+    elif bpm > 120:
+        print("Classificação: Crítico")
+        critico += 1
+        criticos_consecutivos += 1  # soma na sequência
+
+    else:
+        print("Valor fora do padrão")
+        criticos_consecutivos = 0
+
+    # condição de parada
+    if criticos_consecutivos == 3:
+        print("\n Três estados críticos consecutivos detectados!")
+        break
+
+     # relatório final
+    print("\n===== RELATÓRIO =====")
+    print("Total de leituras:", total_leituras)
+    print("Normal:", normal)
+    print("Alerta:", alerta)
+    print("Crítico:", critico)
